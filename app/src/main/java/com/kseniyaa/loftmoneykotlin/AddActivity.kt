d25d6b6c4a0f40b677302500b300a7b71adff634
KaeniyaAndrevna <9216057@gmail.com>
2018-08-30 21:13:45 +0300

    Add tabs

--- app/src/main/java/com/kseniyaa/loftmoneykotlin/AddActivity.kt
+++ app/src/main/java/com/kseniyaa/loftmoneykotlin/AddActivity.kt
@@ -2,8 +2,8 @@ package com.kseniyaa.loftmoneykotlin
 
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
-import kotlinx.android.synthetic.main.activity_add.*
 import com.kseniyaa.loftmoneykotlin.utils.afterTextChanged
+import kotlinx.android.synthetic.main.activity_add.*
 
 class AddActivity : AppCompatActivity() {
 
@@ -11,9 +11,7 @@ class AddActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_add)
 
-
         et_name.afterTextChanged(btn_add,et_name,et_price)
         et_price.afterTextChanged(btn_add,et_name,et_price)
-
     }
 }
